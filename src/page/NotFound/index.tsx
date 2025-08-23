b869470868ccd514a8a36f2b9dfa6e137486e0c9
Garik Martirosyan <garikmartirosyan@Noutbuk-Garik.local>
2025-08-24 01:13:19 +0400

    404 page update

--- src/page/NotFound/index.tsx
+++ src/page/NotFound/index.tsx
@@ -1,18 +1,24 @@
 import styles from "./NotFound.module.scss";
 
-
 const NotFound = () => {
-  const arr = Array.from({length: 9}, (_, i) => i)
-  
+  const arr = Array.from({ length: 9 }, (_, i) => i);
+
   return (
     <div className={styles.container}>
-      <div className={styles.half}>404</div>
+      <div className={styles.half}>
+        <p style={{ marginRight: "200px" }}>404</p>
+      </div>
       <div>
         {arr.map((i) => (
-          <div key={i} className={`${styles.circle} ${i % 2 === 1 ? styles.black : ""}`}></div>
+          <div
+            key={i}
+            className={`${styles.circle} ${i % 2 === 1 ? styles.black : ""}`}
+          ></div>
         ))}
       </div>
-      <div className={styles.half}>Not Found</div>
+      <div className={styles.half}>
+        <p style={{ marginLeft: "200px" }}>Not Found</p>
+      </div>
     </div>
   );
 };
